c9ccfc4116ec81d371531c698b69a4d784d9d055
Omar <omar.dlsg@gmail.com>
2018-03-12 21:57:43 -0700

    Set up start.js and app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const routes = require('./routes/index');
+
+const app = express();
+
+// middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+// // If that above routes didnt work, we 404 them and forward to error handler
+// app.use(errorHandlers.notFound);
+
+// // One of our error handlers will see if these errors are just validation errors
+// app.use(errorHandlers.flashValidationErrors);
+
+// // Otherwise this was a really bad error we didn't expect! Shoot eh
+// if (app.get('env') === 'development') {
+//     /* Development Error Handler - Prints stack trace */
+//     app.use(errorHandlers.developmentErrors);
+// }
+
+// // production error handler
+// app.use(errorHandlers.productionErrors);
+
+module.exports = app;
---
+++ start.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// environmental variables 
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.error(`MONGOOSE ERROR → ${err.message}`);
+});
+
+
+// import models 
+
+// start app 
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
