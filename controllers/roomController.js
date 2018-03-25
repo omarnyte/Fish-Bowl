b30dfa9cf6e8591e05e5c304c4cbf3ebad93c397
Omar <omar.dlsg@gmail.com>
2018-03-24 17:12:44 -0700

    Create GET /api/room/:roomId and POST api/room routes

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 const Room = mongoose.model('Room');
 
-exports.getRoom = (req, res) => {
-    res.send('getting room');
+exports.getRoom = async (req, res) => {
+    const room = await Room.findOne({ slug: req.params.roomId });
+    if (room) {
+        res.json(room);
+    } else {
+        // flash no room found 
+        res.send('no room found');
+    }
+};
+
+exports.createRoom = async (req, res) => {
+    const room = await (new Room());
+    room.players[0] = req.body.username;
+
+    // create random, unique slug
+    let slug = Math.floor(Math.random() * (9999 - 1000) + 1000).toString();
+    while (Room.find({slug})) {
+        slug = Math.floor(Math.random() * (9999 - 1000) + 1000).toString();
+    }
+    rooms.slug = slug;
+
+    room.save();
+    res.json(room)
 };
 
