d4901fed3cc77363363ac6b00f4aee04f7bca10f
Omar <omar.dlsg@gmail.com>
2018-03-25 17:39:14 -0700

    Add boostrap CDN and add container-fluid class to App div

---
+++ frontend/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+    <div className="container-fluid">
+        <img src="logo.png" alt="Fish Bowl Logo" className="img-responsive"></img>
+    </div>
+);
+
+export default App;
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 // import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
+import App from './app';
+
 // add Provider when Redux is set up
 const Root = () => (
         <HashRouter>
-            <h1>Hello</h1>
+            <App />
         </HashRouter>
 );
 
