2352ad2219bdea084b23c33ad2613a2c33127933
Omar <omar.dlsg@gmail.com>
2018-03-27 13:52:35 -0700

    Add has-error class to input when display name is too long

--- frontend/components/enterRoom.jsx
+++ frontend/components/enterRoom.jsx
@@ -16,13 +16,15 @@ class EnterRoom  extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-
-    
     handleInputChange(e) {
+        console.dir(e.target)
         const displayName = e.target.value;
         this.setState({ displayName })
         if (displayName.length >= 20) {
+            e.target.parentElement.classList.add('has-error');
             console.log('too long!');
+            e.target.classList.add('input-invalid');
+
         } 
     }
     
@@ -43,18 +45,21 @@ class EnterRoom  extends React.Component {
 
         return (
             <div>
-                <form className="form-control" onSubmit={this.handleSubmit}>
-                    <label> Enter a display name
-                        <input 
-                            type="text" 
-                            value={displayName}
-                            onChange={this.handleInputChange}
-                        />
+                <form className="form-group has-feedback">
+                    <label className="control-label" >Please Enter a Display Name</label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        value={displayName}
+                        onChange={this.handleInputChange}
+                        id="display-name-input" aria-describedby="display-name-input"></input>
+                    <div className="col-xs-10">
                         <button className="btn btn-default">
-                            { path === "/new-room" ? "Create Room" : "Join Room" }
+                            {path === "/new-room" ? "Create Room" : "Join Room"}
                         </button>
-                    </label>
+                    </div>
                 </form>
+
             </div>
         )
 
@@ -62,3 +67,21 @@ class EnterRoom  extends React.Component {
 };
 
 export default EnterRoom;
+
+{/* <div class="has-success has-feedback">
+    <form className="form-control" onSubmit={this.handleSubmit}>
+        <label> Enter a display name</label>
+        <input
+            type="text"
+            className="form-control"
+            value={displayName}
+            onChange={this.handleInputChange}
+        />
+        <div className="col-xs-10">
+            <button className="btn btn-default">
+                {path === "/new-room" ? "Create Room" : "Join Room"}
+            </button>
+        </div>
+    </form>
+</div> */}
+    
